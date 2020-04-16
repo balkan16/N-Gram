1923eff359900305527ebe88b767aff9a7d27fce
balkan16 <57933796+balkan16@users.noreply.github.com>
2020-04-16 20:44:38 +0700

    Add files via upload

---
+++ main sementara/file_eks2.h
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+typedef struct node {
+    char data[100]; 
+    struct node *next;
+}file_tubes;
+
+
+void openfile(file_tubes *f, char *namafile);
+void filedinamic(FILE *file, file_tubes *current, char *temp);
+void printlist(file_tubes *f); 
+
+
+
+//fungsi open file
+void openfile(file_tubes *f, char *namafile){
+	file_tubes *current = f;
+	char temp[5000];
+	FILE *file;
+	
+	//open file
+	file = fopen(namafile, "r");
+	
+	if (file == NULL) { //jika file tidak ada
+		printf("error!! file tidak ditemukan!\n");
+		current->data[0] = 0;
+		current->next = NULL;
+	}
+	else {
+		filedinamic(file, current, temp);
+		fclose(file);
+	}
+}
+
+//fungsi menyimpan isi file dalam dinamic array
+void filedinamic(FILE *file, file_tubes *current, char *temp){
+	int i, j=0, k, p, q;
+	char temp2[5000];
+	const char s[2] = " ";
+	
+	while (feof(file)==0){ //jika isi sudah dibaca semua
+		fgets(temp,5000,file); //mengambil 1 baris (yg dipisahkan enter (\n)), asumsi maksimal 1 baris 5000 karakter
+		j = strlen(temp); //panjang 1 baris yang diambil
+		k = 0; p = 0; q = 0;
+		
+		for (i=0;i<j;i++){
+			if (temp[i] == '\t'){ //jika ada tab diubah menjadi spasi
+				temp[i] = ' ';
+			}
+		}
+		
+		for (i=0;i<j;i++){
+			if (temp[i] == ' '){ //jika ada k spasi, asumsi jumlah kata adalah k+1
+				if (i!=0 && temp[i+1] != '\n' && temp[i+1] != ' ' && q == 1){ //jika ada spasi double/ diawal/ diakhir
+					k++;
+				}
+			}
+			else if (temp[i] == '\n'){
+				temp[i] = 0;
+				if (k == 0){ //jika ada double \n atau pemisahan paragraf
+					p = 1;
+				}
+			}
+			else { //untuk menemukan karakter pertama yg bukan spasi
+				q = 1;
+			}
+		}
+		
+		if (feof(file) != 0 && temp[0] == 0){ //jika di akhir teks terdapat baris kosong
+			p = 1;
+		}
+		
+		if (p==0){
+			strcpy(temp2, strtok(temp,s)); //mengambil 1 kata dr temp yg dipisahkan oleh spasi (s)
+			strcpy(current->data, temp2); //memasukan data ke dalam dinamic array
+			current->next = NULL;
+			i = 0;
+			while (i < k){ //k = jumlah spasi = jumlah kata - 1
+				current->next =(file_tubes*) malloc(sizeof(file_tubes));
+				strcpy(temp2, strtok(NULL,s)); //mengambil 1 kata dr temp (sebelumnya) yg dipisahkan oleh spasi (s)
+				strcpy(current->next->data, temp2); //memasukan data ke dalam dinamic array
+				current = current->next;
+				current->next = NULL;
+				i++;
+			}
+			if (feof(file)==0){ //jika file blm dibaca semua
+				//inisiasi untuk menyambung kan dengan kata pertama di baris selnjutnya
+				current->next =(file_tubes*) malloc(sizeof(file_tubes));
+				current = current->next;
+				current->next = NULL;
+			}
+		}
+		
+		for (i=0;i<j;i++){ 
+			temp[i] = 0; //membersihkan isi temp jika sewaktu-waktu isi baris kosong tempnya juga kosong
+		}
+	}
+}
+
+//fungsi pengecekan isi linked list
+void printlist(file_tubes *f){
+    file_tubes *current=f;
+
+    while(current != NULL && current->data[0] != 0){ //sampai data terbaca semua atau data terakhir adalah data kosong yg diakibatkan pengolahan sebelumnya
+        printf("%s\n", current->data);
+        current=current->next;
+    }
+}
---
+++ main sementara/menu.c
@@ -0,0 +1,78 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include "file_eks2.h"
+
+//Judul dan deskripsi program
+void deskripsi()
+{
+	printf("=======================================");
+	printf(" >>>>>>>RANDOM STRINGS FROM WORDS N-GRAM<<<<<<< ");
+	printf("=================================\n\n");
+	printf(" Deskripsi Program: \n\n");
+	printf(" Program akan memberikan keluaran berupa string acak yang didapat dari\n membuat n-gram dari inputan file eksternal yang berupa text.");
+	printf("Input dari\n program ini akan berupa, file ekstenal berupa text, n yang berupa bi-\n langan dalam pengamabilan n kata dan N kata acak yang diambil dari kata\n yang terbentuk dari n-gram.");
+	printf(" Program akan kembali meminta jumlah kata ran-\n dom setelah memberikan keluaran. Jika pengguna tidak ingin memberikan\n masukan, program akan beralih ke pemilihan file referensi.");
+	printf("Jika pengguna\n tidak ingin mengganti file referensi, program akan berakhir.");
+	printf("\n-----------------------------------------------------------------------------");
+}
+
+
+//menu pada program
+void menu()
+{
+	file_tubes *f;
+	f =(file_tubes*) malloc(sizeof(file_tubes));
+	char namafile[100];
+	int input = 9,n,N;     //inisialisasi nilai input sebarang
+	deskripsi();
+	
+	while(input != 0)
+	{
+	printf("\n\n Menu:\n ");
+	printf("0. Exit\n ");
+	printf("1. Memasukkan nama file\n ");
+	printf("2. Memasukkan n dalam pengambilan n-gram\n ");
+	printf("3. Memasukkan N buah kata acak yang ingin dicetak\n ");
+	printf("Input:");
+	scanf("%d", &input);
+
+	
+	
+	
+	if ((input < 0) || (input > 3))
+        printf("\nInputan Salah");
+    else if (input == 1){	
+    printf(" Masukkan nama file:");
+    scanf(" %[^\n]%*c",namafile);
+    printf(" %s", namafile);
+	openfile(f, namafile);
+   
+
+	}
+	else if (input == 2){
+		printf(" Masukkan n pengambilan kata pada n-gram: ");
+		scanf(" %d", &n);
+		printf(" %d",n);
+	}
+	else if (input == 3){
+		printf(" Masukkan N kata yang ingin dicetak: ");
+		scanf(" %d",&N);
+		printf(" %d",N);
+	}
+	else
+    	printf(" program selesai");
+    	printlist(f);
+	}
+
+			
+}
+
+
+//fungsi utama 
+int main()
+{
+
+menu();
+return 0;
+} 
